828473f98136de77c7f43348675ca63a63d4bc8c
Chrolo <Chrolo.san@gmail.com>
2017-03-05 14:34:54 +0000

    Removed debug call

--- chrolo.LineBreaker.lua
+++ chrolo.LineBreaker.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------
 -----------------------    Line breaker   ---------------------------------
 ---------------------------------------------------------------------------
--- Purpose: Split up a line by it's \N characters, maintaining position 
+-- Purpose: Split up a line by it's \N characters, maintaining position
 -- 			and current parameters up to that point.
 --
 --
@@ -41,12 +41,12 @@ end
 -- Macros to register:	--
 --------------------------
 function SplitUpLines(subs, sel)
-	
+
 	local offset=0;
 	for _, i in ipairs(sel) do --foreach subtitle line of selection
-	
+
 		local line = subs[i+offset];
-		
+
 		--get the line sizing information:
 		local t_width, t_height, sub_parts = chLib.getTextBound(subs, line);
 		--calculate the co-ordinates of the line
@@ -54,13 +54,13 @@ function SplitUpLines(subs, sel)
 		local info = chLib.getLineInfo(subs, line);
 		local align = info["an"]
 		local rotation = info["frz"]
-		
+
 		rotation = rotation + 90
-		
+
 		--Split the line by occurences of '\N'
 		local t_lines = chLib.string_split(line.text,"\\N");
-		
-		
+
+
 		local new_lines={};
 		local dx,dy = 0,0	--initial pos offsets are 0
 		--start position of offset based of alignment:
@@ -72,65 +72,63 @@ function SplitUpLines(subs, sel)
 		elseif align%3==0 then
 			ori_pos = coords[2]		--3,6,9 are calculated off top right
 		end
-		
-		
+
+
 		--process each t_line
 		for i,i_line in ipairs(t_lines) do
-			--DEBUG
-			aegisub.debug.out(string.format("Current_pos (%g,\t%g)\n",ori_pos[1]-dx,ori_pos[2]+dy))
-			
+
 			--calculate new line position
 			if math.floor((align-1)/3) == 1 then --line is align 4,5,6; need to add half line height before determining position
 				dx, dy = unpack( chLib.matrix_sum( {dx,dy} , { chLib.polar_to_cartesian(sub_parts[i]["height"]/2, rotation)} ) )
 			elseif math.floor((align-1)/3) == 0 then --line is align 1,2,3; need to add line height before determining position
 				dx, dy = unpack( chLib.matrix_sum( {dx,dy} , { chLib.polar_to_cartesian(sub_parts[i]["height"], rotation)} ) )
 			end
-			
-			
+
+
 			--calc the position
 			if info["move_xy"] ~= nil then
 				sub_parts[i][1]["params"]["move"] = {ori_pos[1]-dx,ori_pos[2]+dy,ori_pos[1]-dx+info["move_xy"][1],ori_pos[2]+dy+info["move_xy"][2]}
 			else
 				sub_parts[i][1]["params"]["pos"] = {ori_pos[1]-dx,ori_pos[2]+dy}
 			end
-			
-			
+
+
 			--add any remaining line height
 			if math.floor((align-1)/3) == 1 then --line is align 4,5,6; need to add half line height after determining position
-				dx, dy = unpack( chLib.matrix_sum( {dx,dy} , { chLib.polar_to_cartesian(sub_parts[i]["height"]/2, rotation)} ) ) 
+				dx, dy = unpack( chLib.matrix_sum( {dx,dy} , { chLib.polar_to_cartesian(sub_parts[i]["height"]/2, rotation)} ) )
 			elseif math.floor((align-1)/3) == 2 then --line is align 7,8,9; need to add line height after determining position
 				dx, dy = unpack( chLib.matrix_sum( {dx,dy} , { chLib.polar_to_cartesian(sub_parts[i]["height"], rotation)} ) )
 			end
-			
-			
+
+
 			-- update tags
 			--if i_line ~="" then --only add a new line if line text isn't blank
 			if i_line ~="" then --need to clean out tags, but can't remember the function and i'm on a flight so I can't look it up.
-				
+
 				local insertion = {}
-				
+
 				for key,data in pairs(line) do --add default data to line: (copy timing and such from original line)
 					insertion[key] = data;
 				end
 				insertion.text = chLib.add_params_to_line(i_line, sub_parts[i][1]["params"]);
-				
+
 				table.insert(new_lines,insertion) --insert the data
 			end
 		end
-		
-		
+
+
 		--write changes back to file:
 		for x,y in pairs(new_lines) do
 			subs.insert(i+offset, new_lines[x]);
 			offset=offset+1;	--increase line offset
 		end
-		
+
 		subs.delete(i+offset);		--remove the old line
 		offset=offset-1;				--minus the initial line
-		
+
 	end
-	
+
 	return sel
-	
+
 end
 aegisub.register_macro(menu_embedding..script_name, script_description, SplitUpLines)
